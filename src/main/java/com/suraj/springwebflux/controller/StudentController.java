b3efd8e65b88b0e2639929f889d227903a52ab0f
Suraj Kala <surajkala2001@gmail.com>
2025-08-29 22:03:04 +0530

    Added Model Class

--- src/main/java/com/suraj/springwebflux/controller/StudentController.java
+++ src/main/java/com/suraj/springwebflux/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.suraj.springwebflux.controller;
 
+import com.suraj.springwebflux.model.Student;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,11 @@ import java.time.Duration;
 @RestController
 public class StudentController {
 
-    @GetMapping(value = "/test", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Integer> getAllIntegers() {
-        return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1));
+    @GetMapping(value = "/students", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Student> getAllIntegers() {
+        return Flux.just(new Student(1,"Abhi",32),
+                new Student(2,"Suraj",28),
+                new Student(3,"Nikita",26)
+                ).delayElements(Duration.ofSeconds(1)).log();
     }
 }
---
+++ src/main/java/com/suraj/springwebflux/model/Student.java
@@ -0,0 +1,13 @@
+package com.suraj.springwebflux.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+}
